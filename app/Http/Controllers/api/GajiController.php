d0749b9fd333a6cbbea0ec4d0994f8fce25aae33
ZufarMaliki <zufarmaliki10@gmail.com>
2026-08-26 21:33:34 +0700

    gaji controller api

--- app/Http/Controllers/api/GajiController.php
+++ app/Http/Controllers/api/GajiController.php
@@ -4,8 +4,44 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Gaji;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class GajiController extends Controller
 {
-    //
+    public function index()
+    {
+        $gaji = Gaji::with([
+            'karyawan.jabatan.departemen'
+        ])->get();
+
+        //validasi akun jika tidak terhubung ke data gaji
+        if (!$gaji) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Data gaji tidak ditemukan'
+            ], 404);
+        }
+
+        //Fetch & Mapping data gaji untuk tabel
+        $dataGaji = $gaji->map(function ($gajiItem) {
+            return [
+                'id_karyawan' => $gajiItem->id_karyawan,
+                'bulan' => $gajiItem->bulan,
+                'tahun' => $gajiItem->tahun,
+                'gaji_pokok' => $gajiItem->gaji_pokok,
+                'tunjangan' => $gajiItem->tunjangan,
+                'lembur' => $gajiItem->lembur,
+                'total_gaji' => $gajiItem->total_gaji,
+                'tanggal_bayar' => $gajiItem->tanggal_bayar->locale('id')->translatedFormat('d F Y'),
+                'nama_hrd' => $gajiItem->nama_hrd,
+            ];
+        });
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Data berhasil dimuat',
+            'data' => $dataGaji
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\DashboardController as ApiDashboardController;
+use App\Http\Controllers\api\GajiController as ApiGajiController;
 
 Route::get('/dashboard', [ApiDashboardController::class, 'index'])->name('api.dashboard');
+Route::get('/gaji', [ApiGajiController::class, 'index'])->name('api.gaji');
